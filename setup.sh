cde3c2b3a6e420d91ade6ba32733fc144101a2d2
Dirk Reitz <Dirk.Reitz@flens-it.solutions>
2020-02-18 15:42:09 +0100

    more node modules

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -q -y install mc curl
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo mkdir /home/pc-admin/punchcard
+cd /home/pc-admin/punchcard
 sudo npm install express
-
+sudo npm install body-parser
+sudo npm install promise-mysql
+sudo npm install bluebird
 exit 0
