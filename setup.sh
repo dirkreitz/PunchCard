f8d8e63692cea6dcfc7e2dc6741a4d9dbf28f373
Dirk Reitz <Dirk.Reitz@flens-it.solutions>
2020-02-20 13:56:02 +0100

    add mysql instructions

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update -y
+sudo -E apt-get -q -y update && sudo -E apt-get -q -y upgrade
 sudo apt-get install -y expect
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get -q -y install mysql-server mc curl
@@ -27,6 +27,13 @@ expect eof
 echo "$SECURE_MYSQL"
 
 mysql -uroot -pmysql -e "CREATE DATABASE IF NOT EXISTS punchclock CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+mysql -uroot -pmysql -e "USE punchclock;CREATE TABLE IF NOT EXISTS devicetable (ID int(16) NOT NULL, device varchar(128) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+mysql -uroot -pmysql -e "USE punchclock;CREATE TABLE IF NOT EXISTS timetable (ID int(16) NOT NULL, name varchar(128) NOT NULL, deviceID int(16) NOT NULL, check_type int(4) NOT NULL, UNIX_TIMESTAMP timestamp NOT NULL DEFAULT current_timestamp(), punch_method varchar(32) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+mysql -uroot -pmysql -e "USE punchclock;ALTER TABLE devicetable ADD PRIMARY KEY (ID), ADD UNIQUE KEY device (device);";
+mysql -uroot -pmysql -e "USE punchclock;ALTER TABLE timetable ADD PRIMARY KEY (ID), ADD KEY appID (deviceID);";
+mysql -uroot -pmysql -e "USE punchclock;ALTER TABLE devicetable MODIFY ID int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;";
+mysql -uroot -pmysql -e "USE punchclock;ALTER TABLE timetable MODIFY ID int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;";
+mysql -uroot -pmysql -e "USE punchclock;ALTER TABLE timetable ADD CONSTRAINT fk_appID FOREIGN KEY (deviceID) REFERENCES devicetable (ID), ADD CONSTRAINT fk_name FOREIGN KEY (deviceID) REFERENCES devicetable (ID);COMMIT;";
 
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -38,6 +45,7 @@ sudo npm install body-parser
 sudo npm install mysql
 
 sudo apt-get purge -y expect
+sudo apt -y autoremove
 
 wget https://raw.githubusercontent.com/dirkreitz/PunchCard/master/routes.js
 
